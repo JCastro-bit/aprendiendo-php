05ad952688b55c781c133e94f27fc5fcdbf5c906
Javier Castro <javiercastro27@outlook.es>
2023-04-11 17:27:30 -0600

    🔄 Se actualizo la ruta de las imagenes

--- modulos/header.php
+++ modulos/header.php
@@ -1,3 +1,8 @@
+<?php
+    $host = $_SERVER['HTTP_HOST'];
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' : 'http://';
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -7,23 +12,34 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="57x57" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="http://localhost:86/aprendiendo-php/assets/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="http://localhost:86/aprendiendo-php/assets/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="http://localhost:86/aprendiendo-php/assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="http://localhost:86/aprendiendo-php/assets/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="http://localhost:86/aprendiendo-php/assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="http://localhost:86/aprendiendo-php/assets/favicon/manifest.json">
+    
+
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/manifest.json">
+
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="http://localhost:86/aprendiendo-php/assets/favicon/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?php echo $protocol . $host; ?>/aprendiendo-php/assets/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+
+    <!-- Metatags -->
+
+    <meta name="description" content="Aprende PHP con el Máster en PHP, donde cubrimos PHP 8, bases de datos MySQL, SQL, maquetación web, POO, MVC, librerías y frameworks como Laravel, Symfony y WordPress. Adquiere habilidades full-stack.">
+    <meta property="og:title" content="Máster en PHP - Aprende PHP y Desarrollo Web Full-Stack">
+    <meta property="og:image" content="<?php echo $protocol . $host; ?>/aprendiendo-php/assets\img\aprendiendo-php.jpg">
+    <meta property="og:type" content="website">
+
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
