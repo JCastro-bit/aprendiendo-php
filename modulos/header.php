10562340410cbc19cc7bf9d5e86e5bce4663dc6c
Javier Castro <javiercastro27@outlook.es>
2023-04-15 09:58:37 -0600

    🔄 Replace logo icon

--- modulos/header.php
+++ modulos/header.php
@@ -76,8 +76,7 @@
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="<?php echo $protocol . $host; ?>"
             class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <span class="fs-4">&nbsp&nbspAprendiendo</span>
-            <img src="<?php echo $protocol . $host; ?>\assets\img\logo-php.png" />
+            <img src="<?php echo $protocol . $host; ?>\assets\img\logo-php.png"  width="120" alt="logo-php" />
         </a>
 
         <ul class="nav nav-pills">
